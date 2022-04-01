dac50facdd8e3adf9e5c21dace1c8c214e4325f4
Juan Pablo Saldivar Rojas <jprojas@stefanini.com>
2022-04-01 15:26:24 -0300

    API to login

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AccountController } from './controllers/account/account.controller';
 import { AppController } from './controllers/app.controller';
 import { AppService } from './services/app.service';
 
 @Module({
   imports: [HttpModule, ConfigModule.forRoot()],
-  controllers: [AppController],
+  controllers: [AppController, AccountController],
   providers: [AppService, ConfigService],
 })
 export class AppModule {}
